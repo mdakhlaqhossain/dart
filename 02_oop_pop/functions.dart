415f56e20ba5fc1e9db7f3c63ff342238f1c6ea4
Md. Akhlaq Hossain <70303638+mdakhlaqhossain@users.noreply.github.com>
2024-08-16 14:25:06 +0000

    Progress

--- 02_oop_pop/functions.dart
+++ 02_oop_pop/functions.dart
@@ -1,11 +1,27 @@
 //pop=procedural/functional oriented programming
 void main(){
-  var classVar=myClass(); //creating object
-  print(classVar.classVar1);
+  var classVar=myClass(); // creating object
+  classVar.printNam("Akash");
+  classVar.printNam("Batash");
+  
+  var printS1 = classVar.printSum(4,6);
+  print(printS1);
+  var printS2 = classVar.printSum(5,1);
+  print(printS2);
 }
 
 class myClass{
-  void printName(){ // declaration
-    print("Alom"); //definition
+
+  myClass(){ // default constractor
+    print("Class Object Created");
   }
+  
+  void printNam(String name){ // declaration
+    print(name); //definition
+  }
+  int printSum(int a, int b){ // declaration
+    int sum=a+b;
+    return sum;
+  }
+  
 }
--- 02_oop_pop/objects.dart
+++ 02_oop_pop/objects.dart
@@ -1,9 +1,9 @@
 //oop=object oriented programming
 void main(){
-  var classVar=Human(); //creating object
+  var classVar=Human(); // creating object
   print(classVar.classVar1);
 }
 
-class Human{
+class Human{ // class instance
   var classVar1=2;
 }
